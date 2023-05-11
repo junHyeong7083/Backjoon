05dfbf2fbfe4048184dbff9e8ec65b5457023c57
junHyeong7083 <127365389+junHyeong7083@users.noreply.github.com>
2023-05-12 00:25:21 +0900

    [Bronze II] Title: 공 바꾸기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.cc
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n,m;
+    cin >> n >> m;
+    int arr[101] = { 0 };
+    for (int i = 1; i <= 100; ++i)
+    {
+        arr[i] = i;
+    }
+    while (m--)
+    {
+        int a, b,temp = 0;
+        cin >> a >> b;
+        temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+    for (int i = 1; i <= n; ++i)
+    {
+        cout  << arr[i]<< " ";
+    }
+
+    return 0;
+}
