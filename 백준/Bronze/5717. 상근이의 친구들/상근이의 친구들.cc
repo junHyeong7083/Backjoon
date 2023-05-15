78cede9ba73fa493daf556d373db58489fe78f82
junHyeong7083 <127365389+junHyeong7083@users.noreply.github.com>
2023-05-16 01:15:02 +0900

    [Bronze IV] Title: 상근이의 친구들, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+#include<string.h>
+using namespace std;
+
+int main() 
+{
+	while (1)
+	{
+		int a, b;
+		cin >> a >> b;
+		if (a == 0 && b == 0)
+			break;
+		cout << a + b << endl;
+	}
+}
