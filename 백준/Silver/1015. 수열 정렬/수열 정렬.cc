9715ddad4816e43006354e71dfcb0ced4d8bf2d3
junHyeong7083 <127365389+junHyeong7083@users.noreply.github.com>
2023-05-24 15:07:10 +0900

    [Silver IV] Title: 수열 정렬, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/1015. 수열 정렬/수열 정렬.cc
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<vector>
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+
+int n;
+int a[1001], b[1001], ck[1001];
+vector <int> answer;
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        b[i] = a[i];
+    }
+    sort(b, b + n);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (a[i] == b[j] && !ck[j]) {
+                ck[j] = 1;
+                answer.push_back(j);
+                break;
+            }
+        }
+    }
+    for (auto p : answer) cout << p << ' ';
+
+    return 0;
+}
