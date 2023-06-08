f5f39aa1c0981490f8e6fe29c12f6dce38093109
junHyeong7083 <127365389+junHyeong7083@users.noreply.github.com>
2023-06-08 22:40:29 +0900

    [Bronze V] Title: Rook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "  ___  ___  ___" << endl;
+    cout << "  | |__| |__| |" << endl;
+    cout << "  |           |" << endl;
+    cout << "   \\_________/" << endl;
+    cout << "    \\_______/" << endl;
+    cout << "     |     |" << endl;
+    cout << "     |     |" << endl;
+    cout << "     |     |" << endl;
+    cout << "     |     |" << endl;
+    cout << "     |_____|" << endl;
+    cout << "  __/       \\__" << endl;
+    cout << " /             \\" << endl;
+    cout << "/_______________\\" << endl;
+
+    return 0;
+}
