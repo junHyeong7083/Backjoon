80bd3fcebff28c5153394fdeb0e9964ca4e5b873
junHyeong7083 <127365389+junHyeong7083@users.noreply.github.com>
2023-07-28 02:14:33 +0900

    [Bronze I] Title: 한조서열정리하고옴ㅋㅋ, Time: 448 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14659. 한조서열정리하고옴ㅋㅋ/한조서열정리하고옴ㅋㅋ.cc
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using	namespace std;
+
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	vector <int> arr(n);
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> arr[i];
+	}
+	int result = 0;
+	for (int e = 0; e < n; ++e)
+	{
+		int check = 0;
+		for (int k = e + 1; k < n; ++k)
+		{
+			if (arr[e] > arr[k])
+			{
+				check++;
+				result = max(result, check);
+			}
+			else
+				break;
+
+		}
+	}
+	cout << result;
+
+	return 0;
+}
