f4ce18e887e58ba02874883f225d433760207739
junHyeong7083 <127365389+junHyeong7083@users.noreply.github.com>
2023-07-30 00:51:31 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include<string>
+#include<algorithm>
+#include<map>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cout.tie(NULL);
+    cin.tie(NULL);
+    
+    string a;
+    while (getline(cin, a))
+    {
+        cout << a << endl;
+    }
+
+    return 0;
+}
