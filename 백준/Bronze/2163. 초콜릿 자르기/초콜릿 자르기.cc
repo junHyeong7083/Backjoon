1294dc71fce0ce299428afe56110058a3dfcfa56
junHyeong7083 <127365389+junHyeong7083@users.noreply.github.com>
2024-01-16 01:01:06 +0900

    [Bronze I] Title: 초콜릿 자르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.cc
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<set>
+#include<string>
+#include<queue>
+#include<stack>
+#include<algorithm>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(NULL);
+	std::cout.tie(NULL);
+	int n, m;
+	cin >> n >> m;
+	cout << n * m - 1;
+  
+    return 0;
+}
