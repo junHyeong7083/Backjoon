d5bde2773cddf8b88f52508deff10cc808ef1db8
junHyeong7083 <127365389+junHyeong7083@users.noreply.github.com>
2024-01-17 17:04:42 +0900

    [Gold V] Title: 입국심사, Time: 60 ms, Memory: 2920 KB -BaekjoonHub

---
+++ 백준/Gold/3079. 입국심사/입국심사.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include<vector>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <set>
+#include <map>
+
+using namespace std;
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  int n,m;
+  cin >> n >>m;
+  
+  vector<int> v;
+  
+  for(int e = 0; e < n; ++e)
+    {
+      int x;
+      cin >> x;
+      v.push_back(x);
+    }
+
+  sort(v.begin(),v.end());
+  
+  long long start = 1;
+  long long end =(long long)m*v.back();
+  while(start <= end)
+    {
+      long long mid = (start+end)/2;
+      long long sum = 0;
+      for(int e=0; e< v.size(); ++e)
+          {
+            sum += mid/v[e];
+            if(sum > m)
+              break;
+          }
+        if(sum < m)
+            start = mid  +1;
+        else
+           end = mid - 1;
+      
+    }
+  cout << start;
+
+  return 0;
+}
