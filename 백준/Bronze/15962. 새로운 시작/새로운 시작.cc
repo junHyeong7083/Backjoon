f442c84831cf1ba488b7ec8ed78cf6bda051ccbd
junHyeong7083 <127365389+junHyeong7083@users.noreply.github.com>
2024-01-18 23:37:39 +0900

    [Bronze V] Title: 새로운 시작, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15962. 새로운 시작/새로운 시작.cc
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+#include<cmath>
+#include<string>
+#include<algorithm>
+#include<stack>
+#include<queue>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(NULL);
+    std::cout.tie(NULL);
+
+
+    cout << "파이팅!!";
+
+    return 0;
+
+}
