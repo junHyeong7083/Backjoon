f1f71351e931d084e89219b3455f894f08405591
junHyeong7083 <127365389+junHyeong7083@users.noreply.github.com>
2024-01-23 06:45:25 +0900

    [Gold V] Title: 강의실 배정, Time: 56 ms, Memory: 4484 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.cc
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<vector>
+#include<cmath>
+#include<string>
+#include<algorithm>
+#include<stack>
+#include<queue>
+#include<utility>
+#include<limits>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(NULL);
+    std::cout.tie(NULL);
+   
+   /*
+    n
+    s1 , t1
+
+   */
+
+    int n;
+    cin >> n;
+    
+    vector<pair<int, int>>v(n);
+    for (int e = 0; e < n; ++e)
+        cin >> v[e].first >> v[e].second;
+
+    sort(v.begin(), v.end());
+    int cnt = 1;
+    priority_queue<int, vector<int>, greater<int>> pq;
+    pq.push(v[0].second);
+
+    for (int e = 1; e < n; ++e)
+    {
+        pq.push(v[e].second);
+        if (v[e].first >= pq.top())
+            pq.pop();
+    }
+    cout << pq.size();
+
+    
+
+
+    return 0;
+}
