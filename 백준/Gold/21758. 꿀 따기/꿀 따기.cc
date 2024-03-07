5c2cb8838d20e42fa5f1e27fe7fed520495e8853
junHyeong7083 <127365389+junHyeong7083@users.noreply.github.com>
2024-03-07 16:29:13 +0900

    [Gold V] Title: 꿀 따기, Time: 0 ms, Memory: 2020 KB, Score: 11 point -BaekjoonHub

---
+++ 백준/Gold/21758. 꿀 따기/꿀 따기.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  int N;
+  cin >> N;
+
+  vector<int> v(N+1);
+  for(int i = 1; i <= N; ++i)
+  {
+    cin >> v[i];
+  }
+
+  vector<int>sum(N);
+  for(int i = 1; i<=N; ++i)
+    {
+      sum[i] = sum[i-1] + v[i];
+    }
+
+  int res = 0;
+  for(int i = 2; i <N; ++i)
+    {
+      res = max(res,2*sum[N]-v[1]-v[i]-sum[i]);
+      res = max(res, sum[N-1]-v[i]+sum[i-1]);
+      res = max(res,sum[i]-v[1]+sum[N-1]-sum[i-1]);
+    }
+
+  cout << res;
+  return 0;
+}
